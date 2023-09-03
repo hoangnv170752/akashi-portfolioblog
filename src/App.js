c585b60674cadb3249b146e09dcc204e95ff6547
Hoang Nguyen Van <hoang.nv.ral@gmail.com>
2023-09-03 15:00:11 +0700

    article detail

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+// import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
 import HomePage from './pages/home/HomePage';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className='App font-opensans'>
-      <HomePage />
+      <Routes>
+        <Route index path="/" element={<HomePage />} /> 
+      </Routes>
     </div>
   );
 }
